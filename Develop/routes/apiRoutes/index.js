bf4c2aac5db32b600ee7618225490f4d35565f0b
adrian <sargentamp149@gmail.com>
2021-02-04 13:24:54 -0500

    update to htmlRoutes

---
+++ Develop/routes/apiRoutes/index.js
@@ -0,0 +1,34 @@
+const router = require("express").Router()
+const fs = require("fs");
+const {createNewNote, deleteNote, getNotes} = require("../../lib/notes")
+const {v4: uuidv4} = require('uuid');
+
+router.get("/notes", (req, res) => {
+    console.log('get notes')
+    getNotes().then(results => {
+        if (req.query) {
+            res.json(results);
+        }
+    })
+});
+
+router.post("/notes", (req, res) => {
+    getNotes().then(results => {
+        req.body.id = uuidv4()
+        const newNote = createNewNote(reqq.body, results)
+
+        res.json(newNote)
+    })
+})
+
+//delete function 
+
+router.delete("/notes/:id", (req, res)=> {
+    getNotes().then(results =>{
+        const id = req.params.id
+        const newNote = deleteNote(id, results)
+        res.json(newNote)
+    })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require("express");
-
-const apiRoutes = require("../routes/apiRoutes");
-
-const htmlRoutes = require("../routes/htmlRoutes");
-
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const PORT = process.env.PORT || 8080;
-
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.use("/api, apiRoutes");
-app.use("/, htmlRoutes");
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 
-app.listen(PORT, () =>  console.log('App listening ON Port: ${PORT}'));
+app.listen(PORT, () => { 
+    console.log('API server now on Port 3001!');
+});
